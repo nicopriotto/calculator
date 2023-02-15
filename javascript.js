31e8cbae1d2261f389c7ad3d146b2b0794b91837
nicopriotto <nico.priotto@gmail.com>
2023-02-15 04:29:15 -0300

    Write operational functions

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Operational Functions
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else {
+        console.error("Operator Unknown");
+    }
+}
